dc6ba174ab54c4bde9b6e3ae0b8265d2ae27104c
Matthew Howells <matthowells@icloud.com>
2025-06-05 12:36:50 -0700

    change code of conduct message

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 document.addEventListener('mousemove', function (e) {
     const logo = document.querySelector('.parallax-logo');
-    const speed = 50;
+    const speed = 15;
 
-    const x = (window.innerWidth / 2 - e.pageX) / speed;
-    const y = (window.innerHeight / 2 - e.pageY) / speed;
+    const x = (window.innerWidth / 10 - e.pageX) / speed;
+    const y = (window.innerHeight / 10 - e.pageY) / speed;
 
     logo.style.transform = `translate(${x}px, ${y}px)`;
 });
